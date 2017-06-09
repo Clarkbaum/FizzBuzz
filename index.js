c8067c4927cba40fd6ed2e1663cdb272cdb28fce
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-06-09 16:13:15 -0500

    index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+//FizzBuzz
+//Print out numbers 1 to 100
+//if the number is divisible by 3, print Fizz 
+//if by 5, print Buzz.
+//if by both print FizzBuzz
+
+var max = 100;
+var min = 1;
+
+for (var i = min; i <= max; i++) {
+  if(i % 3 === 0 && i % 5 === 0) {
+    console.log('i', i, 'FizzBuzz');
+  } else if (i % 3 === 0) {
+    console.log('i', i, 'Fizz');
+  } else if (i % 5 === 0) {
+    console.log('i', i, 'Buzz');
+  }
+}
